4974e5e94016188408eee839de62405c49a548bb
xiongjj <xiongjjjjj@outlook.com>
2019-09-21 17:42:53 +0800

    配置数据库

--- src/main/java/com/xj/luckymoney/Luckymoney.java
+++ src/main/java/com/xj/luckymoney/Luckymoney.java
@@ -1,8 +1,66 @@
 package com.xj.luckymoney;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
 /**
  * Created by xj
  * 2019-09-21 16:54
  **/
+@Entity
 public class Luckymoney {
+
+    @Id
+    @GeneratedValue//自增
+    private Integer id;
+
+    private BigDecimal money;
+
+    /**
+     * 发送方
+     */
+    private String producer;
+
+    /**
+     * 接收方
+     */
+    private String consumer;
+
+    public Luckymoney() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getConsumer() {
+        return consumer;
+    }
+
+    public void setConsumer(String consumer) {
+        this.consumer = consumer;
+    }
 }
